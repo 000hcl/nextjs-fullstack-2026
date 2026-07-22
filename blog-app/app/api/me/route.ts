85718a9fbb41bf9320830d9a45827a0249817dee
000hcl <mnrjohansson@gmail.com>
2026-07-22 15:55:56 +0300

    ex 19

--- blog-app/app/api/me/route.ts
+++ blog-app/app/api/me/route.ts
@@ -1,30 +1,3 @@
-// import { NextRequest, NextResponse } from "next/server"
-// import { getServerSession } from "next-auth"
-// import { authOptions } from "../../../lib/auth"
-// import { addNote } from "../../services/notes"
-// import { revalidatePath } from "next/cache"
-
-// export const POST = async (req: NextRequest) => {
-//   const session = await getServerSession(authOptions)
-//   if (!session) {
-//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
-//   }
-
-//   const body = await req.json()
-//   const { content, important = false } = body
-
-//   if (!content || content.length < 10) {
-//     return NextResponse.json(
-//       { error: "Content must be at least 10 characters" },
-//       { status: 400 },
-//     )
-//   }
-
-//   await addNote(content, important)
-//   revalidatePath("/notes")
-//   return NextResponse.json({ success: true }, { status: 201 })
-// }
-
 import { NextRequest, NextResponse } from "next/server"
 import { getUserByAPIToken } from "@/app/services/users"
 
