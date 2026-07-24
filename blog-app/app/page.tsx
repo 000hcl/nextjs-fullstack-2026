9cfb4a1b647d4d7be34517bca001e5522421d98c
000hcl <mnrjohansson@gmail.com>
2026-07-24 11:51:53 +0300

    ex 22

--- blog-app/app/page.tsx
+++ blog-app/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+import Homepage from "@/app/markdown/homepage.mdx"
+
 const Home = () => {
   return (
     <div className="max-w-2xl mx-auto p-6">
-      <h2 className="text-2xl font-bold mb-4">blog app</h2>
-      <h3 className="text-xl font-bold mb-4">home</h3>
+      <div className="markdown">
+        <Homepage />
+      </div>
     </div>
+
   )
 }
+
 export default Home
