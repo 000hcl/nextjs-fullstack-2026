37c3be9aecc41a765ef80b11806b35568b22b132
000hcl <mnrjohansson@gmail.com>
2026-07-24 19:52:10 +0300

    testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  workers: 1,
   use: {
     baseURL: "http://localhost:3000",
+  },
+
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
   },
 });
